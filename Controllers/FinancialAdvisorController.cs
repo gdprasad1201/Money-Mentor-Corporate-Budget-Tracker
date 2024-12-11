34ae0119674339cfa63bfd66a1b01a3bf66a705f
cyrusgomeswork <chgomes@crimson.ua.edu>
2024-12-10 23:45:24 -0600

    commit for ai

---
+++ Controllers/FinancialAdvisorController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Expense_Tracker.Services;
+
+namespace Expense_Tracker.Controllers
+{
+    public class FinancialAdvisorController : Controller
+    {
+        private readonly OpenAIService _openAIService;
+
+        public FinancialAdvisorController(OpenAIService openAIService)
+        {
+            _openAIService = openAIService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            await _openAIService.GetMovieReviews();
+            return View();
+        }
+    }
+}
